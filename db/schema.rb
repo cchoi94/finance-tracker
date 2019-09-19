79906f4033f6758b2bb095889fcf3db0a8b4a521
Cody Choi <cchoi_2294@hotmail.com>
2019-09-19 18:54:06 -0400

    changed welcome from scaffold to controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_220621) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "welcomes", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
