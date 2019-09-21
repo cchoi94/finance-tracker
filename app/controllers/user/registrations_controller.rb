8338dda5858b5e30bf937738f287ecce82b1c86f
Cody Choi <cchoi_2294@hotmail.com>
2019-09-20 21:08:23 -0400

    updated devise parameter sanitizer

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_paramenters
+  before_action :configure_permitted_parameters
 
   protected
 
-  def configure_permitted_paramenters
-    devise_parameter_sanitizer.permit(:signup, keys: %i[first_name last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
   end
+
 end
